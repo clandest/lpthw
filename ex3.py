06264b5608df464794824014e0ac1849efe1384d
Devyn Challman <devyn.challman@gmail.com>
2014-05-11 17:33:47 -0700

    How calculations work

---
+++ ex3.py
@@ -0,0 +1,43 @@
+# Learning about math operators, addition (+) subtraction (-) division (/) multiplication (*) modulus (%) greater than (>) less than (<) and so forth
+
+# Prints out a string
+print 'I will now count my chickens:'
+
+# Print out string and an addition/division equation
+print 'Hens', 25.0 + 30.0 / 6.0
+# Print out a string and a subtraction,multiply,modulus equation
+print 'Roosters', 100.0 - 25.0 * 3.0 % 4.0
+
+# Print out a string
+print 'Now I will count the eggs:'
+
+# Print out an addition,subtraction,modulus, division equation
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Print out a string with addtion, less than, minus
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+# print out addtion, less than, minus
+print 3.0 + 2.0 < 5.0 - 7.0
+
+# Print out string and addition
+print "What is 3 + 2?", 3.0 + 2.0
+
+# Print out string and subtraction
+print "What is 5 - 7?", 5.0 - 7.0
+# Print out a string
+print "Oh, that's why it's False."
+
+# Print out a string
+print "How about some more."
+
+# Print out a string and greater than negative number
+print "Is it greater?", 5.0 > -2.0
+# Print out string and greater than equal to negative number
+print "Is it greater or equal?", 5.0 >= -2.0
+# Print out string and less than equal to negative number
+print "Is it less or equal?", 5.0 <= -2.0
+
+
+print 7.0 / 4.0
+print 7 / 4
