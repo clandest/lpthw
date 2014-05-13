d5607ef93daafaf0f9de183c759b6c33ea60b42a
Devyn Challman <devyn.challman@gmail.com>
2014-05-12 18:16:58 -0700

    simple input with raw_input()

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh",
+weight = raw_input()
+
+print "So you're %r tall, %r heavy, %r heavy." % (age, height, weight)
